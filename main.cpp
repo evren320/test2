4a2961369b92f7e16741cf3c0fb7a74377cfa502
Evren Erce <evren320@hotmail.com>
2022-02-04 13:33:26 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hi Earth" << std::endl;
     return 0;
 }
